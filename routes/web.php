1b2886322057bac97033004aecd459b13e8dec41
stanislas musoz <2150901@cegepoutaouais.qc.ca>
2025-11-19 13:35:59 -0500

    Créer un controller, un routage et des méthodes pour contrôler la requête

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\FormController;
 
@@ -18,7 +19,7 @@ Route::get('/',  function () {
     return view('index');
 });
 
-Route::get('/submit', function () {
+Route::post('/submit', function () {
     $name = request("nom");
     $password = request("password");
     $commentaire = request("commentaire");
@@ -28,3 +29,6 @@ Route::get('/submit', function () {
         "commentaire" => $commentaire
     ]);
 });
+
+//Utiliser un controller pour gérer le routage
+Route::resource("controller",Controller::class);
